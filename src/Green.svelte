1ef2da8140d85110e6e578bc6c39807a1141a811
Alex Galays <boubiyeah@hotmail.com>
2021-04-12 17:30:57 +0200

    test formation

--- src/Green.svelte
+++ src/Green.svelte
@@ -6,7 +6,7 @@
   export let params: RouteParams<AppRouter, 'users'>
 </script>
 
-<div in:fade={{ duration: 120 }}>
+<div in:fade={{ duration: 200 }}>
   <h2>Users</h2>
 
   <p>{params.date.toDateString()}</p>
@@ -14,7 +14,7 @@
 
 <style>
   div {
-    background-color: #66ff66;
+    background-color: gray;
     color: white;
     width: var(--section-size);
     height: var(--section-size);
